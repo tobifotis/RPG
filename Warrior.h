b18de4cc3cfc700eb73b822f1b360d341358f805
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-04-14 10:31:04 -0500

    fix: minor indentation errors

--- Warrior.h
+++ Warrior.h
@@ -5,11 +5,12 @@
 
 class Warrior : public Player
 {
-    public:
+public:
     Warrior(string name, Race race) : Player(name, race, 200, 0)
-    {}
+    {
+    }
 
-    string attack() const 
+    string attack() const
     {
         return "I will destroy you with my sword, foul demon!";
     }
